9f7a081d09833e5f3d581b38803b1a43f2e5b179
Crispin R <crispinr003@gmail.com>
2021-10-21 19:13:22 +0530

    Remove warnings

--- src/Components/Delete.js
+++ src/Components/Delete.js
@@ -2,23 +2,25 @@ import React from "react";
 import { useHistory } from "react-router";
 import styles from "../css_modules/show.module.css";
 
-export const Delete=({id})=>{
-    const history=useHistory()
-    const deleteHandler=()=>{
-        fetch(`/api/delete/${id}`,{
-            method:'POST',
-            body:JSON.stringify({id:id}),
-            headers:{
-                'Content-type':'application/json; charset=UTF-8'
-            }
-        })
-        .then((resp)=>resp.json())
-        .then((data)=>{
-            console.log(data)
-            history.push('/')
-        })
-    }
-    return (
-        <button className={styles.custmBtn} role="button" onClick={deleteHandler}>Delete</button>
-    )
-}
+export const Delete = ({ id }) => {
+  const history = useHistory();
+  const deleteHandler = () => {
+    fetch(`/api/delete/${id}`, {
+      method: "POST",
+      body: JSON.stringify({ id: id }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        history.push("/");
+      });
+  };
+  return (
+    <button className={styles.custmBtn} onClick={deleteHandler}>
+      Delete
+    </button>
+  );
+};
